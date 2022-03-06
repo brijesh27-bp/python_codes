158cdb381bbac802e006676f685bd1697501ad56
Brijesh <brijeshpant7@gmail.com>
2022-03-06 09:54:09 +0530

    My first commit

---
+++ cal.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 17 15:59:12 2020
+
+@author: user hp
+"""
+def newfunc(name):
+    for letter in name:
+         if letter == 'a':
+             print('You got right!')
+         else:
+             print('OOpssss')
