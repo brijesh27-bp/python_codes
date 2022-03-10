00bf9e7eda6eeaafb0faf39f0ca32a8922bc3be1
Brijesh <brijeshpant7@gmail.com>
2022-03-10 09:18:18 +0530

    new fresco code

---
+++ fresco1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 10 08:00:30 2022
+
+@author: user hp
+"""
+
+def stringmethod(para,sp1):
+    
+    
+    for char in sp1:
+        para = para.replace(char,'')
+    word1 = para
+    print(word1)
+    
+        
+        
