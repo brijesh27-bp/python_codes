51b07cf8b6737439f460048860ccc31c91e26bad
Brijesh <brijeshpant7@gmail.com>
2022-03-12 11:11:42 +0530

    Repeat letter code

---
+++ char_three.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 11:00:58 2022
+
+@author: user hp
+"""
+def paper_doll(text):
+    
+    new_text =''
+    
+    for letter in text:
+        
+        new_text += letter*3
+    return new_text 
